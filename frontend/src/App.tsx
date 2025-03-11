34c673acd7550418fcda43daff6b1e76beec9909
dwivediansh7 <ansh.dwivedi.ug23@nsut.ac.in>
2025-03-12 00:21:19 +0530

    Changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import WhoWeArePage from './pages/WhoWeArePage';
-import CarsPage from './pages/CarsPage';
-import Footer from './components/Footer';
-import ContactPage from './pages/ContactPage';
-import PackagesPage from './pages/PlacesPage';
-import PackageItinerary from './pages/PackageItinerary';
-import AdminPanel from './admin/AdminPanel';
-import Car from './admin/car/Car';
-import Place from './admin/package/Place';
-import AddCar from './admin/car/AddCar';
-import './App.css'
-import CarList from './admin/car/CarList';
-import MainPage from './pages/MainPage';
-import AddPlace from './admin/package/AddPlace';
-import PlaceList from './admin/package/PlaceList';
-import Package from './admin/package/Package';
-import AddPackage from './admin/package/AddPackage';
-import PackageList from './admin/package/PackageList';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import WhoWeArePage from "./pages/WhoWeArePage";
+import CarsPage from "./pages/CarsPage";
+import Footer from "./components/Footer";
+import ContactPage from "./pages/ContactPage";
+import PackagesPage from "./pages/PlacesPage";
+import PackageItinerary from "./pages/PackageItinerary";
+import AdminPanel from "./admin/AdminPanel";
+import Car from "./admin/car/Car";
+import Place from "./admin/package/Place";
+import AddCar from "./admin/car/AddCar";
+import "./App.css";
+import CarList from "./admin/car/CarList";
+import MainPage from "./pages/MainPage";
+import AddPlace from "./admin/package/AddPlace";
+import PlaceList from "./admin/package/PlaceList";
+import Package from "./admin/package/Package";
+import AddPackage from "./admin/package/AddPackage";
+import PackageList from "./admin/package/PackageList";
+import FAQsPage from "./pages/FAQsPage";
+import BookingsPage from "./pages/BookingsPage";
 
 const App = () => {
   return (
@@ -32,6 +34,8 @@ const App = () => {
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/packages" element={<PackagesPage />} />
             <Route path="/itinerary/:placeId" element={<PackageItinerary />} />
+            <Route path="/faqs" element={<FAQsPage />} />
+            <Route path="/bookings" element={<BookingsPage />} />
 
             <Route path="/admin" element={<AdminPanel />} />
             <Route path="/admin/cars" element={<Car />} />
@@ -44,9 +48,14 @@ const App = () => {
             <Route path="/admin/places/update-place" element={<PlaceList />} />
 
             <Route path="/admin/places/:id" element={<Package />} />
-            <Route path="/admin/places/packages/add-package" element={<AddPackage />} />
-            <Route path="/admin/places/packages/update-package" element={<PackageList />} />
-
+            <Route
+              path="/admin/places/packages/add-package"
+              element={<AddPackage />}
+            />
+            <Route
+              path="/admin/places/packages/update-package"
+              element={<PackageList />}
+            />
           </Routes>
         </main>
         <Footer />
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,12 @@
-import { Link } from 'react-router-dom';
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaYoutube, FaInstagram } from 'react-icons/fa';
-import '../styles/footer.css';
+import { Link } from "react-router-dom";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaYoutube,
+  FaInstagram,
+} from "react-icons/fa";
+import "../styles/footer.css";
 
 const Footer = () => {
   return (
@@ -9,11 +15,19 @@ const Footer = () => {
         <div className="footer-section">
           <h3>Know Travel Explorers</h3>
           <ul>
-            <li><Link to="/about">Home</Link></li>
-            <li><Link to="/agent">Who We Are</Link></li>
-            <li><Link to="/careers">Packages</Link></li>
-            {/* <li><Link to="/careers">Cars</Link></li> */}
-            <li><Link to="/contact">Contact Us</Link></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/who-we-are">Who We Are</Link>
+            </li>
+            <li>
+              <Link to="/packages">Packages</Link>
+            </li>
+            {/* <li><Link to="/cars">Cars</Link></li> */}
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
           </ul>
         </div>
 
@@ -22,16 +36,26 @@ const Footer = () => {
           <ul>
             {/* <li><a href="/terms"></a></li> */}
             {/* <li><a href="/privacy">Privacy Policy</a></li> */}
-            <li><a href="/bookings">Bookings & Refund Policy</a></li>
-            <li><a href="/faqs">FAQ's</a></li>
+            <li>
+              <Link to="/bookings">Bookings & Refund Policy</Link>
+            </li>
+            <li>
+              <Link to="/faqs">FAQ's</Link>
+            </li>
           </ul>
         </div>
 
         <div className="footer-section">
           <h3>Customer Reviews</h3>
           <ul>
-            <li><a href="/testimonials">Testimonials</a></li>
-            <li><a href="mailto:info@travelexplorer.in">Email ID: tvistatour@gmail.com</a></li>
+            <li>
+              <a href="/testimonials">Testimonials</a>
+            </li>
+            <li>
+              <a href="mailto:info@travelexplorer.in">
+                Email ID: tvistatour@gmail.com
+              </a>
+            </li>
           </ul>
         </div>
 
@@ -44,7 +68,9 @@ const Footer = () => {
             <a href="#" className="social-icon"><FaYoutube /></a>
             <a href="#" className="social-icon"><FaInstagram /></a>
           </div> */}
-          <Link to="/admin" className="admin-login-btn">Login as Admin</Link>
+          <Link to="/admin" className="admin-login-btn">
+            Login as Admin
+          </Link>
         </div>
       </div>
       <div className="footer-bottom">
@@ -54,4 +80,4 @@ const Footer = () => {
   );
 };
 
-export default Footer; 
+export default Footer;
